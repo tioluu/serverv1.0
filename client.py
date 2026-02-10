4e58c965a6107eb2385670237959b4851b9c0c7c
tolu <babalola.it@gmail.com>
2026-02-10 01:24:31 +0100

    add more headers

--- client.py
+++ client.py
@@ -11,12 +11,14 @@ path = "/shorten"
 data = urllib.parse.urlencode({"url": "https://github.com/tiolu"})
 
 headers = {
+    "User-Agent": "MyCustomClient/1.0",
+    "Accept-Language": "en-US,en;q=0.9",
     "Content-type": "application/x-www-form-urlencoded",
     "Authorization": f"Bearer {os.getenv('SPOO_API_KEY')}",
 }
 
 conn = http.client.HTTPConnection(host)
-conn.request("POST", path, body=data)
+conn.request("POST", path, body=data, headers=headers)
 
 response = conn.getresponse()
 print("Status:", response.status, response.reason)
